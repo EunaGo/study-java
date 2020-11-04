72b1d2a6f2e12caee65379616a20ccaef6ec5840
EunaGo <gumagoo8@gmail.com>
2020-11-04 23:45:17 +0900

    가운데 글자 가져오기

--- Programmers/src/Lv1/middle_character_import.java
+++ Programmers/src/Lv1/middle_character_import.java
@@ -1,5 +1,4 @@
 package Lv1;
-
 /*
 [문제 설명]
 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
@@ -7,14 +6,7 @@ package Lv1;
 [제한 사항]
 s는 길이가 1 이상, 100이하인 스트링입니다.
 */
-
-public class middle_character_import {
-
-	public static void main(String[] args) {
-
-		System.out.println(solution("abc"));
-
-	}
+public class Middle_character_import {
 	
 	public static String solution(String s) {
 		String answer = "";
@@ -28,4 +20,8 @@ public class middle_character_import {
 		return answer;
 	}
 
+	public static void main(String[] args) {
+		System.out.println(solution("abc"));
+	}
+	
 }
