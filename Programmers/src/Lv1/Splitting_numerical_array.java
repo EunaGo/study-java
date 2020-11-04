4dd1f5d98ceadb881d480416c8b6436a2627ed76
EunaGo <gumagoo8@gmail.com>
2020-11-04 23:45:05 +0900

    나누어 떨어지는 숫자 배열

--- Programmers/src/Lv1/Splitting_numerical_array.java
+++ Programmers/src/Lv1/Splitting_numerical_array.java
@@ -1,8 +1,6 @@
 package Lv1;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-
 /*
 [문제 설명]
 array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환하는 함수, solution을 작성해주세요.
@@ -14,16 +12,7 @@ arr은 자연수를 담은 배열입니다.
 divisor는 자연수입니다.
 array는 길이 1 이상인 배열입니다.
 */
-
 public class Splitting_numerical_array {
-
-	public static void main(String[] args) {
-		
-		int[] arr = {2, 36, 1, 3};
-		
-		solution(arr, 1);
-		
-	}
 	
     public static int[] solution(int[] arr, int divisor) {
 
@@ -31,9 +20,7 @@ public class Splitting_numerical_array {
     	int cnt = 0;
     	
     	for(int i=0; i<arr.length; i++) {
-    		if(arr[i] % divisor == 0) {
-    			cnt++;
-    		}
+    		if(arr[i] % divisor == 0) cnt++;
     	}
     	
     	answer = new int[cnt];
@@ -52,12 +39,13 @@ public class Splitting_numerical_array {
     	} else {
     		Arrays.sort(answer);
     	}
-    	
-    	for(int i=0; i<answer.length; i++) {
-    		System.out.println(answer[i]);
-    	}
-        
+     
         return answer;
     }
+    
+	public static void main(String[] args) {
+		int[] arr = {2, 36, 1, 3};
+		solution(arr, 1);
+	}
     
 }
