322b90b47f04e5100709681426dce2d1bb957646
EunaGo <gumagoo8@gmail.com>
2020-11-12 17:56:37 +0900

    카펫

---
+++ Programmers/src/Lv2/Carpet.java
@@ -0,0 +1,17 @@
+package Lv2;
+
+public class Carpet {
+	
+    public static int[] solution(int brown, int yellow) {
+        int[] answer = {};
+        
+        
+        
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		solution(24, 24);
+	}
+
+}
