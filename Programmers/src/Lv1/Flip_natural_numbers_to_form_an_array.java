1876b289d532c0da4c90445826b0bc70a2080e53
Luna <gumagoo8@gmail.com>
2021-05-04 18:12:48 +0900

    자연수 뒤집어 배열로 만들기

---
+++ Programmers/src/Lv1/Flip_natural_numbers_to_form_an_array.java
@@ -0,0 +1,28 @@
+package Lv1;
+/*
+[문제 설명]
+자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+
+[제한 조건]
+n은 10,000,000,000이하인 자연수입니다.
+*/
+public class Flip_natural_numbers_to_form_an_array {
+	
+    public static int[] solution(long n) {
+        int[] answer = {};
+        String num = String.valueOf(n);
+        String[] arr = num.split("");
+        
+        for(int i=0; i<arr.length; i++){
+        	//answer[i] = Integer.parseInt(arr[arr.length-i-1]);
+        	answer[i] = 1; 
+        }
+        
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		System.out.println(solution(12345));
+	}
+
+}
