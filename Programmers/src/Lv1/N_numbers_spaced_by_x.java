b132983577f84284ca319db3b39789329bf1ac42
Luna <gumagoo8@gmail.com>
2021-05-11 17:32:38 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ Programmers/src/Lv1/N_numbers_spaced_by_x.java
@@ -0,0 +1,23 @@
+package Lv1;
+/*
+[문제 설명]
+함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. 
+다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+
+[제한 조건]
+x는 -10000000 이상, 10000000 이하인 정수입니다.
+n은 1000 이하인 자연수입니다.
+*/
+public class N_numbers_spaced_by_x {
+
+    public static long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        for(int i=1; i<=n; i++) for(int j=0; j<i; j++) answer[i-1] = answer[i-1] + x;
+        return answer;
+    }
+    
+	public static void main(String[] args) {
+		solution(2, 5);
+	}
+
+}
