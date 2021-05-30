355d90c0b42ea180fe990dcac5eb9a58fd6f0b97
EunaGo <gumagoo8@gmail.com>
2021-05-30 17:28:49 +0900

    서울에서 김서방찾기

--- Programmers/src/Lv1/Find_Kim_Seobang_in_Seoul.java
+++ Programmers/src/Lv1/Find_Kim_Seobang_in_Seoul.java
@@ -19,9 +19,8 @@ public class Find_Kim_Seobang_in_Seoul {
     public static String solution(String[] seoul) {
         String answer = "";
         
-        for(int i=0; i<seoul.length; i++) {
+        for(int i=0; i<seoul.length; i++) 
         	if(seoul[i].equals("Kim")) answer = "김서방은 "+i+"에 있다";
-        }
         
         return answer;
     }
